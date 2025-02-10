3fb43e650f8e17d06355bee7f7e6081ee5fb4028
Thomas Nakas <tnakas@3-A-2.42heilbronn.de>
2025-02-10 21:44:51 +0100

    fixing typos containers still are on the reestarting mode

--- srcs/wordpress/wp-config.php
+++ srcs/wordpress/wp-config.php
@@ -2,4 +2,4 @@
 define('DB_NAME', getenv('WORDPRESS_DB_NAME'));
 define('DB_USER', getenv('WORDPRESS_DB_USER'));
 define('DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD'));
-define('DB_HOSH', getenv('WORDPRESS_DB_HOSH'));
+define('DB_HOST', getenv('WORDPRESS_DB_HOST'));
